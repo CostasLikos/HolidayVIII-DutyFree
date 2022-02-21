276277cb2945d1b568efadd163ddb0f6480b5fca
Costas Likos <costaslikos@gmail.com>
2022-02-21 21:29:39 +0200

    init

---
+++ HolidayVII_DutyFree/Program.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Linq;
+
+public class Kata
+{
+    public static int DutyFree(int normPrice, int Discount, int hol) => (int)Math.Floor(hol / (normPrice * (Discount / 100.0)));
+
+}
